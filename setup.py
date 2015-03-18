eeebee72c8f11c92fb9755d14ec75c94bb90ccbf
TomV <tom@viner.tv>
2015-03-18 22:18:05 +0000

    Back to development: 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='my-package',
-    version='0.1.0',
+    version='0.1.1.dev0',
     packages=find_packages('src'),
     package_dir={'': 'src'},
 )
